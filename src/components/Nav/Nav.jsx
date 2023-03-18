cfd92241439a2c7d096b84427b50d555ecaf67e6
maritrini <mariatrinidadsalazarr@gmail.com>
2023-03-17 19:16:22 -0500

    actualizado

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,14 +5,14 @@ export default function Nav(props) {
   return (
     <div>
       <div className={styles.navBarLinks}>
-        <Link to="/about">
-          <h3>ABOUT</h3>
-        </Link>
         <Link to="/home">
           <h3>HOME</h3>
         </Link>
         <Link to="/favorites">
           <h3>FAVORITES</h3>
+        </Link>
+        <Link to="/about">
+          <h3>ABOUT</h3>
         </Link>
       </div>
       <div>
